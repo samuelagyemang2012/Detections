970a63284c33c77ee8c8b5abc5dc49012512379f
samuelagyemang2012 <khermz2012@gmail.com>
2022-04-21 18:18:17 +0800

    video inference error

--- video_inference.py
+++ video_inference.py
@@ -1,21 +1,23 @@
 import os
 import cv2
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from imageio import imread
-import numpy as np
-from matplotlib import pyplot as plt
-from keras_loss_function.keras_ssd_loss import SSDLoss
-from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
-from keras_layers.keras_layer_DecodeDetections import DecodeDetections
-from keras_layers.keras_layer_L2Normalization import L2Normalization
-from keras_loss_function.keras_ssd_loss import SSDLoss
-from ssd_encoder_decoder.ssd_output_decoder import decode_detections
+
+# from tensorflow.keras import backend as K
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# from imageio import imread
+# import numpy as np
+# from matplotlib import pyplot as plt
+# from keras_loss_function.keras_ssd_loss import SSDLoss
+# from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+# from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+# from keras_layers.keras_layer_L2Normalization import L2Normalization
+# from keras_loss_function.keras_ssd_loss import SSDLoss
+# from ssd_encoder_decoder.ssd_output_decoder import decode_detections
+
 
 video_path = "C:/Users/Administrator/Desktop/video.mp4"
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(video_path)
 
 while True:
     res, frame = capture.read()
